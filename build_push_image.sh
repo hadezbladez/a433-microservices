8b1afa97d87f5b5d5296550d721c166d8c17700f
Hansen <hadezbladez@gmail.com>
2024-02-20 11:53:11 +0700

    Add files via upload

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,2 +1,5 @@
 docker build -t item-app:v1 .
 docker image ls
+docker tag item-app:v1 hadezbladez/item-app:v1
+echo $PASSWORD_DOCKER_HUB | docker login -u hadezbladez@gmail.com --password-stdin
+docker push hadezbladez/item-app:v1
